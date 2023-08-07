bcf7f653fe344ab221476a6fca18dee5b4e43b55
Robin <robin.selin@tuni.fi>
2023-08-07 13:28:11 +0300

    added main-function

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,18 @@
-print("Hello world!")
-print("Hello from feature")
-print("Hello from release")
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hello world!")
+    print("Hello from feature")
+    print("Hello from release")
+
+
+# Press the green button in the gutter to run the script.
+main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
