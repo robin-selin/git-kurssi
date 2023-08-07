77f2b1cfaf3c0f7209f52a32cb057c1d43699289
Robin <robin.selin@tuni.fi>
2023-08-07 13:29:46 +0300

    add main-function on feature

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,15 @@
-print("Hello world!")
-print("Hello from feature")
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hello World!")
+    print("Hello from feature")
+
+# Press the green button in the gutter to run the script.
+main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
