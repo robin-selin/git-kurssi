8f95c866f56a5d48f03004a5ee8df8940f036c5f
Robin <robin.selin@tuni.fi>
2023-08-15 15:57:25 +0300

    fix typing on release

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> bcf7f653fe344ab221476a6fca18dee5b4e43b55
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -10,18 +7,9 @@
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
-<<<<<<< HEAD
-    print("Hello World!")
-    print("Hello from feature")
-    print("Hello from release")
-
-=======
     print("Hello world!")
     print("Hello from feature")
     print("Hello from release")
-
-
->>>>>>> bcf7f653fe344ab221476a6fca18dee5b4e43b55
 # Press the green button in the gutter to run the script.
 main()
 
